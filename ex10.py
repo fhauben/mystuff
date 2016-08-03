cfb8873ddf3e51d0520126567183fa1b25e9182b
frank hauben <fhauben@palantir.com>
2016-08-03 10:52:08 -0700

    added ex10

---
+++ ex10.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm slipt\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+print "I'm split\n on a line."
+
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i,
+
+
