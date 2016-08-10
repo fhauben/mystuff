e79e2f9b8a50d6f886becd40b3e9b99f4582aaab
frank hauben <fhauben@palantir.com>
2016-08-10 14:39:17 -0700

    more files

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+ans1 = animals[1]
+print (ans1 + ' python')
+ans2 = animals[2]
+print (ans2 + ' peacock')
+ans3 = animals[0]
+print (ans3 + ' bear')
+ans4 = animals[3]
+print (ans4 + ' kangaroo')
+
