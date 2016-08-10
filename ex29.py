65cda6868450f03e05ce19bf5fc7868ea889ee88
frank hauben <fhauben@palantir.com>
2016-08-09 23:09:20 -0700

    additional exercises

---
+++ ex29.py
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too may cats!  The world is doomed!"
+
+if people > cats:
+	print "Not many cats!  The world is saved!"
+
+if people < dogs:
+	print "The world is filled with dog poop!"
+
+if people > dogs:
+	print "The world is clean!"
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+if people == dogs:
+	print "People are dogs."
+
+
